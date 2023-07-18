04b835e3720949cab31aba3c409edc8dadc0797e
Mohammad Jahanseir <48261288+mjahanseir@users.noreply.github.com>
2023-07-18 17:11:28 -0600

    Project Tax Calculator

---
+++ Project_TaxCalculator/TaxCalculator/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace TaxCalculator
+{
+    partial class frmTaxCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSalary = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 31);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Enter Your Name: ";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(40, 180);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(231, 76);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(246, 47);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(531, 38);
+            this.txtName.TabIndex = 0;
+            this.txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Location = new System.Drawing.Point(246, 102);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(531, 38);
+            this.txtSalary.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(196, 31);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Enter Your Salary: ";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(298, 180);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(231, 76);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(546, 180);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(231, 76);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(45, 299);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(76, 31);
+            this.lblResult.TabIndex = 5;
+            this.lblResult.Text = "Result";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(691, 346);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 49);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "?";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmTaxCalculator
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.ClientSize = new System.Drawing.Size(808, 458);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtSalary);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmTaxCalculator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tax Calculator";
+            this.Load += new System.EventHandler(this.frmTaxCalculator_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtSalary;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.HelpProvider helpProvider1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Project_TaxCalculator/TaxCalculator/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaxCalculator
+{
+    public partial class frmTaxCalculator : Form
+    {
+        public frmTaxCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+          
+            double tax, salary, pureSalary;
+            try
+            {
+                salary = Convert.ToDouble(txtSalary.Text);
+
+                if (salary < 50_000)
+                {
+                    tax = 0;
+                }
+                else if (salary < 100_000)
+                {
+                    tax = 5 * salary / 100;
+                }
+                else if (salary < 200_000)
+                {
+                    tax = 10 * salary / 100;
+                }
+                else if (salary < 3_000_000)
+                {
+                    tax = 15 * salary / 100;
+                }
+                else
+                {
+                    tax = 15 * salary / 100;
+                }
+
+                pureSalary = salary - tax;
+
+                lblResult.Text = txtName.Text + " your tax is: " + tax.ToString() + "\n your payment is: " + pureSalary.ToString();
+                txtName.Focus();
+                txtName.SelectAll();
+
+            }
+            catch
+            {
+                lblResult.Text = "Please Enter Valid input! ";
+                txtSalary.Focus();
+                txtSalary.SelectAll();
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            DialogResult exitConfirm =  MessageBox.Show("Are you sure to exit?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (exitConfirm == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtName.Text = "";
+            txtSalary.Text = "";
+            lblResult.Text = " ";
+            txtName.Focus();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"\t      Salary  <  50_000 => tax = 0 " +
+                            $"\n\n    50_000 < Salary < 100_000  => tax = 5 * salary / 100 " +
+                            $"\n\n   100_000 < salary < 200_000  => tax = 10 * salary / 100  " +
+                            $"\n\n   200_000 < salary < 3_000_000=> tax = 15 * salary / 100   " +
+                            $"\n\n\t      Salary > 3_000_000=> tax = 15 * salary / 100 ");
+        }
+
+        private void frmTaxCalculator_Load(object sender, EventArgs e)
+        {
+
+            txtName_TextChanged(null, null);
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+            if (txtName.Text == "")
+            {
+                btnCalculate.Enabled = false;
+            }
+            else
+            {
+                btnCalculate.Enabled = true;
+            }
+        }
+    }
+}
